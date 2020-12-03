4b41705a8add1387a525092dc9b005952f54af6b
Liamsray22 <eliamsolis2201@gmail.com>
2020-12-02 21:15:21 -0400

    responsive

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,20 +2,27 @@ import React, {useState} from 'react'
 import Search from './Search'
 import PanelAdd from './PanelAdd'
 import '../css/Menu.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faBars,faTimes} from '@fortawesome/free-solid-svg-icons'
 
 
 
 const Menu = (props)=> {  
     
     const [panel,setPanel] = useState(false)
+    const [clicked,setClicked] = useState(false)
     
     function Add(){
         setPanel(!panel)
     }
+    const handleClick =()=>{
+        setClicked(!clicked)
+    }
     return(
 <div className="containerr">
-    <div className="subcontainer">
-        <div className="logo">
+
+    <div className={clicked?"subcontainer_active":"subcontainer"}>
+    <div className="logo">
             <img src="/img/logo.png" width="25%"/>
             {props.title}
         </div>
@@ -27,7 +34,11 @@ const Menu = (props)=> {
         <div className="actions">
             <button onClick={Add} className="button btn-blue">+ Agregar Anime</button>
         </div>
+            
     </div>
+    <div className="menu-icon" onClick={handleClick}>
+                <FontAwesomeIcon icon={clicked ? faTimes:faBars}/>
+            </div>
     {
         panel?
         <PanelAdd oncancel = {Add} onadd={props.onadd}/>
